1890fac2573688dabb43ae325b8413c198754824
abhi-nihr <abhisheik.yarrakrishnappa@nihr.ac.uk>
2024-06-05 14:57:04 +0100

    Add medallion buckets for lakeformation

--- global.tf
+++ global.tf
@@ -10,6 +10,9 @@ variable "names" {
       "environment"        = "dev"
       "app"                = "rddi-data-platform"
       "bucket_name"        = "nihrd-s3-dev-rddi-data-platform-raw"
+      "bucket_name_bronze" = "nihrd-s3-dev-rddi-data-platform-bronze"
+      "bucket_name_silver" = "nihrd-s3-dev-rddi-data-platform-silver"
+      "bucket_name_gold"   = "nihrd-s3-dev-rddi-data-platform-gold"
     }
   }
 }
