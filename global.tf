d4f6d769f8c7a6351d74e4c330223957d55620d4
abhi-nihr <abhisheik.yarrakrishnappa@nihr.ac.uk>
2024-07-11 11:25:24 +0100

    Update brokers to use SSL port

--- global.tf
+++ global.tf
@@ -16,7 +16,7 @@ variable "names" {
       "custom_plugin_name" = "nihrd-dev-rddi-data-platform-s3-sink-connector-plugin"
       "connect_name"       = "nihrd-s3-dev-rddi-data-platform-connect"
       "private_subnet_ids" = ["subnet-036934130e6e171db", "subnet-08301b8a8d127a1e5"]
-      "bootstrap_servers"  = "b-1.nihrdmskdevnsipcluster.z2kr4f.c2.kafka.eu-west-2.amazonaws.com:9092,b-2.nihrdmskdevnsipcluster.z2kr4f.c2.kafka.eu-west-2.amazonaws.com:9092"
+      "bootstrap_servers"  = "b-1.nihrdmskdevnsipcluster.z2kr4f.c2.kafka.eu-west-2.amazonaws.com:9094,b-2.nihrdmskdevnsipcluster.z2kr4f.c2.kafka.eu-west-2.amazonaws.com:9094"
       "msk_security_group" = ["sg-0b80f3f34f0a80e31"]
       "retention_in_days"  = 30
     }
