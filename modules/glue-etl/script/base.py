97cdbbc3db655ba93772e0db52e81bfb8335f2a3
eunanhardypa <50829217+eunanhardypa@users.noreply.github.com>
2024-07-16 14:05:12 +0100

    Update base script working in editor

--- modules/glue-etl/script/base.py
+++ modules/glue-etl/script/base.py
@@ -4,6 +4,7 @@ from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
 from awsglue.job import Job
 import gs_now
 
@@ -17,15 +18,18 @@ def setup(args):
     return input_path,output_path
 
 def transform(input_path, ctx):
-    source_node = ctx.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="parquet", connection_options={"paths": [input_path], "recurse": True}, transformation_ctx="AmazonS3_node1718377480779")
+    AmazonS3_node1718377480779 = ctx.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="parquet", connection_options={"paths": [input_path], "recurse": True}, transformation_ctx="AmazonS3_node1718377480779")
 
     # <!--- PUT YOUR TRANSFORMS HERE -->
     # Source node shoudl attach to your first transform. 
+    
     # Script generated for node Add Current Timestamp
-    AddCurrentTimestamp_node1720445890121 = source_node.gs_now(colName="data_processed_at")
-
+    # gs_now and other glue transforms attach directly to the DataFrame - df
+    df = AmazonS3_node1718377480779.toDF().gs_now(colName="data_run_at") #
+    AddCurrentTimestamp_node1720445890121 = DynamicFrame.fromDF(df,ctx,"timestamp")
     # </>
     # Return your final transform node, this will write it out to the next bucket stage
+    # Returned value must be a DynamicFrame
     return AddCurrentTimestamp_node1720445890121
 
 def main():
@@ -36,10 +40,11 @@ def main():
     job = Job(glueContext)
     job.init(args['JOB_NAME'], args)
     input_path,out_path = setup(args)
-    transformEndpoint = transform(input_path,glueContext)
+    transforms = transform(input_path,glueContext)
 
-    output_node = glueContext.write_dynamic_frame.from_options(frame=transformEndpoint, connection_type="s3", format="glueparquet", connection_options={"path": out_path, "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1720446021764")
+    output_node = glueContext.write_dynamic_frame.from_options(frame=transforms, connection_type="s3", format="glueparquet", connection_options={"path": out_path, "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1720446021764")
     job.commit()
 
 main()
 
+# job.commit()
